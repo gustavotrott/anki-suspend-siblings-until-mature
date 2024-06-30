b74bea7eb8446bc52782d97470b71373d912f440
Gustavo Trott <gustavo@trott.com.br>
2024-06-30 18:25:25 -0300

    fix: The add-on buries same-day siblings in the whole collection

--- __init__.py
+++ __init__.py
@@ -20,15 +20,22 @@ from .tools import (
 ############################################################################### reviewer
 ########################################################################################
 
+config = Config()
+config.load()
+
 
 @gui_hooks.reviewer_did_answer_card.append
 def reviewer_did_answer_card(_, card: Card, ease):
+    # Check if the feature is enabled for the current deck
+    if not config.enabled_for_current_deck:
+        return
+
     isMature = card.ivl > config.unsuspend_on and card.type == CARD_TYPE_REV and card.queue == QUEUE_TYPE_REV
 
     siblings = get_siblings(card)
     for sibling in siblings:
         question = html_to_text_line(sibling.question())
-        willSuspend = isMature == False and sibling.type == CARD_TYPE_NEW and sibling.queue in [QUEUE_TYPE_NEW, QUEUE_TYPE_SIBLING_BURIED]
+        willSuspend = not isMature and sibling.type == CARD_TYPE_NEW and sibling.queue in [QUEUE_TYPE_NEW, QUEUE_TYPE_SIBLING_BURIED]
 
         # Skip if sibling is ahead in the order
         if sibling.ord < card.ord:
@@ -38,58 +45,52 @@ def reviewer_did_answer_card(_, card: Card, ease):
             # print(f"Using scheduler < 3")
             # Remove sibling from curr queue (rev, new or learn)
             if sibling.id in mw.col.sched._revQueue:
-                if willSuspend == False:
-                    mw.col.sched.bury_cards(ids=[sibling.id, ], manual=False)
+                if not willSuspend:
+                    mw.col.sched.bury_cards(ids=[sibling.id], manual=False)
                 with suppress(AttributeError, ValueError):
                     mw.col.sched._revQueue.remove(sibling.id)  # noqa
 
             if sibling.id in mw.col.sched._newQueue:
-                if willSuspend == False:
-                    mw.col.sched.bury_cards(ids=[sibling.id, ], manual=False)
+                if not willSuspend:
+                    mw.col.sched.bury_cards(ids=[sibling.id], manual=False)
                 with suppress(AttributeError, ValueError):
                     mw.col.sched._newQueue.remove(sibling.id)  # noqa
 
             if sibling.id in mw.col.sched._lrnQueue:
-                if willSuspend == False:
-                    mw.col.sched.bury_cards(ids=[sibling.id, ], manual=False)
+                if not willSuspend:
+                    mw.col.sched.bury_cards(ids=[sibling.id], manual=False)
                 with suppress(AttributeError, ValueError):
                     mw.col.sched._lrnQueue.remove(sibling.id)  # noqa
         else:
             # print(f"Using scheduler 3")
             queuedCards = mw.col.sched.get_queued_cards(fetch_limit=10000).cards
             for item in queuedCards:
-                if(item.card.id == sibling.id):
-                    #card = mw.col.get_card(item.card.id)
-                    if willSuspend == False:
-                        mw.col.sched.bury_cards(ids=[sibling.id, ], manual=False)
+                if item.card.id == sibling.id:
+                    if not willSuspend:
+                        mw.col.sched.bury_cards(ids=[sibling.id], manual=False)
 
-
-        # dont take any action if card is RELEARNING (pressed "Again" button)
+        # Don't take any action if the card is RELEARNING (pressed "Again" button)
         if card.type == CARD_TYPE_RELEARNING:
             return
 
         if willSuspend:
-            mw.col.sched.suspend_cards(ids=[sibling.id, ])
+            mw.col.sched.suspend_cards(ids=[sibling.id])
             print(f"Sibling: {question} Suspended!<br>")
-            if (not config.quiet):
+            if not config.quiet:
                 tooltip(f"Sibling: {question} Suspended!<br>")
 
         if isMature and sibling.queue == QUEUE_TYPE_SUSPENDED and sibling.ord == card.ord + 1:
-            mw.col.sched.unsuspend_cards(ids=[sibling.id, ])
-            mw.col.sched.bury_cards(ids=[sibling.id, ], manual=False)
+            mw.col.sched.unsuspend_cards(ids=[sibling.id])
+            mw.col.sched.bury_cards(ids=[sibling.id], manual=False)
             question = html_to_text_line(sibling.question())
             print(f"Sibling: {question} UNSuspended!<br>")
-            if (not config.quiet):
+            if not config.quiet:
                 tooltip(f"Sibling: {question} UNSuspended!<br>")
 
 
 ########################################################################################
 ################################################################ menus and configuration
 ########################################################################################
-
-config = Config()
-config.load()
-
 
 def set_enabled_for_this_deck(checked):
     config.enabled_for_current_deck = checked
